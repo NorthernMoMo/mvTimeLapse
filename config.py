03ba0d412e9165df55a5aa4564f94e4bf01b48c3
NorthernMoMo <moses.ellisjr@hess.com>
2024-05-16 08:57:27 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 API_KEY=""
-CAMERA_SERIAL=""
-time_zone="America/Bogota"
+CAMERA_SERIAL=Q2UV-JPMF-3J5L
+time_zone="America/North Dakota"
 # These settings would be for starting on Feb 8th, 2022 at 11:04:31 (24hr format)
-start_year=2022
-start_month=2
-start_day=8
-start_hour=11
-start_minute=4
-start_second=31
+start_year=2024
+start_month=5
+start_day=16
+start_hour=9
+start_minute=30
+start_second=00
 # Lapse settings
-lapse_interval=10
-lapse_length=120
-lapse_file_name="timelapse.gif"
+lapse_interval=300
+lapse_length=10
+lapse_file_name=kitchentimelapse.gif
